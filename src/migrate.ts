a97af4aae956a300d561e4da3719766cd3389e0f
PeelayMurtuza <peelaymurtuza@gmail.com>
2024-12-17 19:53:08 +0530

    created demo model

---
+++ src/migrate.ts
@@ -0,0 +1,21 @@
+import {LoopbackModelApplication} from './application';
+import { Customer } from './models';
+
+export async function migrate(args: string[]) {
+  const existingSchema = args.includes('--rebuild') ? 'drop' : 'alter';
+  console.log('Migrating schemas (%s existing schema)', existingSchema);
+
+  const app = new LoopbackModelApplication();
+  await app.boot();
+  await app.migrateSchema({existingSchema,models:['Customer', 'Order','Address','Manufacturer']});
+
+  // Connectors usually keep a pool of opened connections,
+  // this keeps the process running even after all work is done.
+  // We need to exit explicitly.
+  process.exit(0);
+}
+
+migrate(process.argv).catch(err => {
+  console.error('Cannot migrate database schema', err);
+  process.exit(1);
+});
---
+++ src/repositories/customer.repository.ts
@@ -0,0 +1,27 @@
+import {inject, Getter} from '@loopback/core';
+import {DefaultCrudRepository, repository, HasManyRepositoryFactory, HasOneRepositoryFactory} from '@loopback/repository';
+import {MysqlconnectionDataSource} from '../datasources';
+import {Customer, CustomerRelations, Order, Address} from '../models';
+import {OrderRepository} from './order.repository';
+import {AddressRepository} from './address.repository';
+
+export class CustomerRepository extends DefaultCrudRepository<
+  Customer,
+  typeof Customer.prototype.customer_id,
+  CustomerRelations
+> {
+
+  public readonly orders: HasManyRepositoryFactory<Order, typeof Customer.prototype.customer_id>;
+
+  public readonly address: HasOneRepositoryFactory<Address, typeof Customer.prototype.customer_id>;
+
+  constructor(
+    @inject('datasources.mysqlconnection') dataSource: MysqlconnectionDataSource, @repository.getter('OrderRepository') protected orderRepositoryGetter: Getter<OrderRepository>, @repository.getter('AddressRepository') protected addressRepositoryGetter: Getter<AddressRepository>,
+  ) {
+    super(Customer, dataSource);
+    this.address = this.createHasOneRepositoryFactoryFor('address', addressRepositoryGetter);
+    this.registerInclusionResolver('address', this.address.inclusionResolver);
+    this.orders = this.createHasManyRepositoryFactoryFor('orders', orderRepositoryGetter,);
+    this.registerInclusionResolver('orders', this.orders.inclusionResolver);
+  }
+}
